9d8bed0df58877ecb0cc12c5bfe6aee967b1b22e
Pedro Mendes <pedrolopesme@gmail.com>
2016-01-20 17:11:37 -0200

    Adding Quotes generator using famous quotes from the classic movies

---
+++ src/test/java/org/jenkinsci/plugins/starwars/plugin/QuotesGeneratorTest.java
@@ -0,0 +1,24 @@
+package org.jenkinsci.plugins.starwars.plugin;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class QuotesGeneratorTest {
+
+	private QuotesGenerator generator = new QuotesGenerator();
+
+	@Test
+	public void testRandomShouldGivesDiferentQuotes() {
+		int totalAttempts = 100;
+		String lastQuote = null;
+		for (int i = 0; i < totalAttempts; i++) {
+			String currQuote = generator.random();
+			if (lastQuote != null && !lastQuote.equals(currQuote)) {
+				return;
+			}
+			lastQuote = currQuote;
+		}
+		fail("Random should give at least 2 different facts in " + totalAttempts + " tries.");
+	}
+}
